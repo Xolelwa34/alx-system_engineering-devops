6b3182c35c75ae6209aae84dd7169d5204e759c7
Xolelwa34 <ciliaswekile@gmail.com>
2024-03-12 09:30:33 -0700

    Text

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #Takes a phone number and a message arguments
-puts ARGV[0].scan(/\[from:(.*)\]\s\[to:(.*)\]\s\[flags:(*)\]/).join(",")
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
