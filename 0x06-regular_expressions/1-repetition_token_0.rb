ad1f1d06922786f34286fa42bd5d85f972e2462d
Xolelwa34 <ciliaswekile@gmail.com>
2024-03-12 06:59:43 -0700

    Repetition token #1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #Regular expression repetition token
-puts AGRV[0].scan(/hbt|2,5/n).join
+puts ARGV[0].scan(/hbt|2,5|n/).join
---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#Repetition that matches the given token
+puts ARGV[0].scan(/hb?tn/).join
